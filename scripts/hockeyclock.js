b1c1de9fcad864c82ab47e460024dc580199a1ec
Sven Hamberg <hamberg.sven@gmail.com>
2024-04-12 16:57:38 +0200

    Formatting + CSS cleanup

--- scripts/hockeyclock.js
+++ scripts/hockeyclock.js
@@ -5,111 +5,113 @@ var m_current = -1;
 var s_current = -1;
 
 
-function flip (upperId, lowerId, changeNumber, pathUpper, pathLower){
-	var upperBackId = upperId+"Back";
+function flip(upperId, lowerId, changeNumber, pathUpper, pathLower) {
+	var upperBackId = upperId + "Back";
 	$(upperId).src = $(upperBackId).src;
 	$(upperId).setStyle("height", "80px");
 	$(upperId).setStyle("visibility", "visible");
-	$(upperBackId).src = pathUpper+parseInt(changeNumber)+".png";
+	$(upperBackId).src = pathUpper + parseInt(changeNumber) + ".png";
 
-	$(lowerId).src = pathLower+parseInt(changeNumber)+".png";
+	$(lowerId).src = pathLower + parseInt(changeNumber) + ".png";
 	$(lowerId).setStyle("height", "0px");
 	$(lowerId).setStyle("visibility", "visible");
 
-	var flipUpper = new Fx.Tween(upperId, {duration: 200, transition: Fx.Transitions.Sine.easeInOut});
+	var flipUpper = new Fx.Tween(upperId, { duration: 200, transition: Fx.Transitions.Sine.easeInOut });
 	flipUpper.addEvents({
-		'complete': function(){
-			var flipLower = new Fx.Tween(lowerId, {duration: 200, transition: Fx.Transitions.Sine.easeInOut});
+		'complete': function () {
+			var flipLower = new Fx.Tween(lowerId, { duration: 200, transition: Fx.Transitions.Sine.easeInOut });
 			flipLower.addEvents({
-				'complete': function(){	
-					lowerBackId = lowerId+"Back";
+				'complete': function () {
+					lowerBackId = lowerId + "Back";
 					$(lowerBackId).src = $(lowerId).src;
 					$(lowerId).setStyle("visibility", "hidden");
 					$(upperId).setStyle("visibility", "hidden");
-					}				});					
-					flipLower.start('height', 80);
-
 				}
 			});
-			flipUpper.start('height', 0);
+			flipLower.start('height', 80);
+
+		}
+	});
+	flipUpper.start('height', 0);
 
 
-			}//flip
+}//flip
 
-			function flipDate (upperId, lowerId, changeNumber, pathUpper, pathLower){
-				var upperBackId = upperId+"Back";
-				$(upperId).src = $(upperBackId).src;
-				$(upperId).setStyle("height", "30px");
-				$(upperId).setStyle("visibility", "visible");
-				$(upperBackId).src = pathUpper+parseInt(changeNumber)+".png";
+function flipDate(upperId, lowerId, changeNumber, pathUpper, pathLower) {
+	var upperBackId = upperId + "Back";
+	$(upperId).src = $(upperBackId).src;
+	$(upperId).setStyle("height", "30px");
+	$(upperId).setStyle("visibility", "visible");
+	$(upperBackId).src = pathUpper + parseInt(changeNumber) + ".png";
 
-				$(lowerId).src = pathLower+parseInt(changeNumber)+".png";
-				$(lowerId).setStyle("height", "0px");
-				$(lowerId).setStyle("visibility", "visible");
+	$(lowerId).src = pathLower + parseInt(changeNumber) + ".png";
+	$(lowerId).setStyle("height", "0px");
+	$(lowerId).setStyle("visibility", "visible");
 
-				var flipUpper = new Fx.Tween(upperId, {duration: 200, transition: Fx.Transitions.Sine.easeInOut});
-				flipUpper.addEvents({
-					'complete': function(){
-						var flipLower = new Fx.Tween(lowerId, {duration: 200, transition: Fx.Transitions.Sine.easeInOut});
-						flipLower.addEvents({
-							'complete': function(){	
-								lowerBackId = lowerId+"Back";
-								$(lowerBackId).src = $(lowerId).src;
-								$(lowerId).setStyle("visibility", "hidden");
-								$(upperId).setStyle("visibility", "hidden");
-								}				});					
-								flipLower.start('height', 30);
+	var flipUpper = new Fx.Tween(upperId, { duration: 200, transition: Fx.Transitions.Sine.easeInOut });
+	flipUpper.addEvents({
+		'complete': function () {
+			var flipLower = new Fx.Tween(lowerId, { duration: 200, transition: Fx.Transitions.Sine.easeInOut });
+			flipLower.addEvents({
+				'complete': function () {
+					lowerBackId = lowerId + "Back";
+					$(lowerBackId).src = $(lowerId).src;
+					$(lowerId).setStyle("visibility", "hidden");
+					$(upperId).setStyle("visibility", "hidden");
+				}
+			});
+			flipLower.start('height', 30);
 
-							}
-						});
-						flipUpper.start('height', 0);
-
-
-						}//flip
+		}
+	});
+	flipUpper.start('height', 0);
 
 
-						function hockeyClock(){
-
-							// get new time
-							now = new Date();
-							d = now.getDate();
-							h = now.getHours();
-							m = now.getMinutes();
-							s = now.getSeconds();
+}//flip
 
 
-							//change pads
+function hockeyClock() {
 
-							if(d != d_current){
-								flipDate('dateUp', 'dateDown', d, './images/faces/upper/', './images/faces/lower/');
-								d_current = d;
-							}
-
-							if( h == 9 && m == 9){
-								h = 991;
-								m = 992;
-							}
-
-							if( h != h_current){
-								flip('hoursUp', 'hoursDown', h, './images/faces/upper/', './images/faces/lower/');
-								h_current = h;
-							}
-
-							if( m != m_current){
-								flip('minutesUp', 'minutesDown', m, './images/faces/upper/', './images/faces/lower/');
-								m_current = m;
-							}
+	// get new time
+	now = new Date();
+	d = now.getDate();
+	h = now.getHours();
+	m = now.getMinutes();
+	s = now.getSeconds();
 
 
-							if (s != s_current){
-								flip('secondsUp', 'secondsDown', s, './images/faces/upper/', './images/faces/lower/');
-								s_current = s;
-							}
+	//change pads
+
+	if (d != d_current) {
+		flipDate('dateUp', 'dateDown', d, './images/faces/upper/', './images/faces/lower/');
+		d_current = d;
+	}
+
+	if (h == 9 && m == 9) {
+		h = 991;
+		m = 992;
+	}
+
+	if (h != h_current) {
+		flip('hoursUp', 'hoursDown', h, './images/faces/upper/', './images/faces/lower/');
+		h_current = h;
+	}
+
+	if (m != m_current) {
+		flip('minutesUp', 'minutesDown', m, './images/faces/upper/', './images/faces/lower/');
+		m_current = m;
+	}
+
+
+	if (s != s_current) {
+		flip('secondsUp', 'secondsDown', s, './images/faces/upper/', './images/faces/lower/');
+		s_current = s;
+	}
 
 
 
 
 
-						}
+}
 
-						setInterval('hockeyClock()', 1000);
+setInterval('hockeyClock()', 1000);
